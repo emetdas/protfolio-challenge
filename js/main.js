3d9c35ecd8ad7d9d9c26be5dcd48b106ef8755c8
emetdas <emet@b2bleadstaff.com>
2021-06-12 18:08:10 +0600

    some major update

--- js/main.js
+++ js/main.js
@@ -1,37 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    logic();
+  logic();
 });
-function logic(){
-// variable
-let nav_link = document.querySelectorAll('.manue > li');
-let nav_manue_icon = document.querySelector('.mobile-icon');
-let manue = document.querySelector('.manue');
-let progress = document.querySelectorAll('.progress');
+function logic() {
+  // variable
+  let nav_link = document.querySelectorAll('.manue > li');
+  let nav_manue_icon = document.querySelector('.mobile-icon');
+  let manue = document.querySelector('.manue');
+  let progress = document.querySelectorAll('.progress');
 
-
-
-
-
-
-// Mobile-navbar-start
-nav_manue_icon.addEventListener('click',(e)=>{
+  // Mobile-navbar-start
+  nav_manue_icon.addEventListener('click', (e) => {
     nav_manue_icon.classList.toggle('active');
     manue.classList.toggle('active');
-});
-// Mobile-navbar-end
-// Nav-link is Clicked the manue is close-start
-    nav_link.forEach((c)=>{
-        c.addEventListener('click',()=>{
-            nav_manue_icon.classList.remove('active');
-            manue.classList.remove('active');
-        });
+  });
+  // Mobile-navbar-end
+  // Nav-link is Clicked the manue is close-start
+  nav_link.forEach((c) => {
+    c.addEventListener('click', () => {
+      nav_manue_icon.classList.remove('active');
+      manue.classList.remove('active');
     });
-// Nav-link is Clicked the manue is close-end
-// Progress-bar-start
-progress.forEach((e)=>{ 
+  });
+  // Nav-link is Clicked the manue is close-end
+  // Progress-bar-start
+  progress.forEach((e) => {
     e.style.width = e.getAttribute('data-progress') + '%';
-});
-// Progress-bar-end
+  });
+  // Progress-bar-end
 
 
 
@@ -39,49 +34,5 @@ progress.forEach((e)=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 }
