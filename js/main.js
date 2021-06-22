afedf5d4ad27040ca412caa27a02bfa0aa60e1fb
emetdas <emet@b2bleadstaff.com>
2021-06-23 00:08:11 +0600

    done

--- js/main.js
+++ js/main.js
@@ -78,6 +78,7 @@ function logic() {
   });
   // Protfolio-part-end
   // testimonial-slider-start
+  
   let sectionIndex = 0;
   taright.addEventListener('click',()=>{
     sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
